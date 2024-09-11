d38c1ad8db533972614d41e54f8c358ceefb53f9
ludoff <ludovicofiore25@gmail.com>
2024-09-11 15:39:34 +0200

    feat: add base files and add controller for routes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,14 +6,15 @@
 @endsection
 
 @section('content')
-<div class="container my-5">
-    <h1>{{ $title }}</h1>
-    <img src="{{ Vite::asset('resources/img/colibri.jpg') }}" alt="" class="img-fluid">
-    <p>
-        {{ $text }}
-    </p>
-</div>
-
+    <div class="container my-5">
+        <h1>Title</h1>
+        <img src="{{ Vite::asset('resources/img/colibri.jpg') }}" alt="" class="img-fluid">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, molestiae? Assumenda vero necessitatibus
+            qui sequi, dolorum sed eligendi praesentium magni quibusdam labore doloribus harum, quaerat officia voluptatum
+            atque, placeat numquam?
+        </p>
+    </div>
 @endsection
 
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PageController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,16 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $title = 'Layout base - HOME';
-    $text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi, iusto dolore eaque laudantium optio, adipisci perspiciatis voluptate obcaecati sit perferendis natus velit ut! Tempora voluptatum ipsam modi rem voluptas.';
-    return view('home', compact('text', 'title'));
-})->name('home');
+// Route::get('/', function () {
 
-Route::get('/chi-siamo', function () {
-    return view('about');
-})->name('about');
+//     return view('home');
+// })->name('home');
 
-Route::get('/contatti', function () {
-    return view('contacts');
-})->name('contacts');
+// Route::get('/chi-siamo', function () {
+//     return view('about');
+// })->name('about');
+
+// Route::get('/contatti', function () {
+//     return view('contacts');
+// })->name('contacts');
+
+
+Route::get('/',[PageController::class, 'index'])->name('home');
+Route::get('/chi-siamo',[PageController::class, 'about'])->name('about');
+Route::get('/contatti',[PageController::class, 'contacts'])->name('contacts');
